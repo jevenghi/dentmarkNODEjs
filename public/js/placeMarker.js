68fddfb4b153da8616a88461fb24d31943c9a17b
Jev Lap <jevenghi@gmail.com>
2024-03-23 22:35:47 +0100

    markers positions fix task page

--- public/js/placeMarker.js
+++ public/js/placeMarker.js
@@ -5,23 +5,23 @@ const isFrontOrRear = (side) => {
   return side.slice(-2) === 'fr' || side.slice(-2) === 're';
 };
 
-export const placeMarker = (
-  side,
-  shape,
-  length,
-  orientationDent,
-  paintDamaged,
-  coords,
-  image,
-) => {
+const markerStyle = (shape, marker, side, coords, orientationDent, w1, w2, h1, h2, x1, x2, y1, y2) => {
+  marker.style.width = isFrontOrRear(side) ? w1 : w2;
+  marker.style.height = isFrontOrRear(side) ? h1 : h2;
+
+  marker.style.left = isFrontOrRear(side) ? `${coords.x - x1}%` : `${coords.x - x2}%`;
+  marker.style.top = isFrontOrRear(side) ? `${coords.y - y1}%` : `${coords.y - y2}%`;
+  if (shape === 'line') {
+    marker.style.borderRadius = '0.8rem';
+    marker.style.transform = `rotate(${orientationDent})`;
+  }
+};
+
+export const placeMarker = (side, shape, length, orientationDent, paintDamaged, coords, image) => {
   const marker = document.createElement('div');
   marker.className = 'marker';
-  marker.style.left = isFrontOrRear(side)
-    ? `${coords.x - 2}%`
-    : `${coords.x - 1}%`;
-  marker.style.top = isFrontOrRear(side)
-    ? `${coords.y - 3.5}%`
-    : `${coords.y - 3}%`;
+  // marker.style.left = isFrontOrRear(side) ? `${coords.x - 2}%` : `${coords.x - 1}%`;
+  // marker.style.top = isFrontOrRear(side) ? `${coords.y - 3.5}%` : `${coords.y - 3}%`;
 
   if (paintDamaged) {
     const markerX = document.createElement('span');
@@ -30,41 +30,62 @@ export const placeMarker = (
   }
 
   if (length === 'small') {
-    // marker.style.background = '#FF5722';
     if (shape === 'nonagon') {
-      marker.style.width = isFrontOrRear(side) ? '1.3rem' : '0.5rem';
-      marker.style.height = isFrontOrRear(side) ? '1.3rem' : '0.5rem';
+      markerStyle('nonagon', marker, side, coords, null, '1.3rem', '0.5rem', '1.3rem', '0.5rem', 2, 0.8, 2.6, 2.6);
     } else if (shape === 'line') {
-      marker.style.width = isFrontOrRear(side) ? '1.5rem' : '0.8rem';
-      marker.style.height = isFrontOrRear(side) ? '0.6rem' : '0.3rem';
-      marker.style.borderRadius = '0.8rem';
-      marker.style.transform = `rotate(${orientationDent})`;
+      markerStyle('line', marker, side, coords, orientationDent, '1.5rem', '0.8rem', '0.6rem', '0.3rem', 2, 1, 1.5, 1.8);
     }
   }
+
   if (length === 'medium') {
-    // marker.style.background = '#FF5722';
     if (shape === 'nonagon') {
-      marker.style.width = isFrontOrRear(side) ? '2rem' : '0.8rem';
-      marker.style.height = isFrontOrRear(side) ? '2rem' : '0.8rem';
+      markerStyle('nonagon', marker, side, coords, null, '2rem', '0.8rem', '2rem', '0.8rem', 2.6, 1, 3.6, 3);
     } else if (shape === 'line') {
-      marker.style.width = isFrontOrRear(side) ? '2.2rem' : '1.4rem';
-      marker.style.height = isFrontOrRear(side) ? '0.8rem' : '0.5rem';
-      marker.style.borderRadius = '0.8rem';
-      marker.style.transform = `rotate(${orientationDent})`;
+      markerStyle('line', marker, side, coords, orientationDent, '2.2rem', '1.4rem', '0.8rem', '0.5rem', 3.2, 2, 1.8, 2.6);
     }
   }
+
   if (length === 'big') {
-    // marker.style.background = '#FF5722';
     if (shape === 'nonagon') {
-      marker.style.width = isFrontOrRear(side) ? '2.6rem' : '1.6rem';
-      marker.style.height = isFrontOrRear(side) ? '2.6rem' : '1.6rem';
+      markerStyle('nonagon', marker, side, coords, null, '2.6rem', '1.6rem', '2.6rem', '1.6rem', 3.4, 2, 5, 5.3);
     } else if (shape === 'line') {
-      marker.style.width = isFrontOrRear(side) ? '2.9rem' : '2.2rem';
-      marker.style.height = isFrontOrRear(side) ? '1.2rem' : '0.8rem';
-      marker.style.borderRadius = '0.8rem';
-      marker.style.transform = `rotate(${orientationDent})`;
+      markerStyle('line', marker, side, coords, orientationDent, '2.9rem', '2.2rem', '1.2rem', '0.8rem', 3.8, 3.2, 2.5, 4.2);
     }
   }
+
+  // if (length === 'small') {
+  //   if (shape === 'nonagon') {
+  //     marker.style.width = isFrontOrRear(side) ? '1.3rem' : '0.5rem';
+  //     marker.style.height = isFrontOrRear(side) ? '1.3rem' : '0.5rem';
+  //   } else if (shape === 'line') {
+  //     marker.style.width = isFrontOrRear(side) ? '1.5rem' : '0.8rem';
+  //     marker.style.height = isFrontOrRear(side) ? '0.6rem' : '0.3rem';
+  //     marker.style.borderRadius = '0.8rem';
+  //     marker.style.transform = `rotate(${orientationDent})`;
+  //   }
+  // }
+  // if (length === 'medium') {
+  //   if (shape === 'nonagon') {
+  //     marker.style.width = isFrontOrRear(side) ? '2rem' : '0.8rem';
+  //     marker.style.height = isFrontOrRear(side) ? '2rem' : '0.8rem';
+  //   } else if (shape === 'line') {
+  //     marker.style.width = isFrontOrRear(side) ? '2.2rem' : '1.4rem';
+  //     marker.style.height = isFrontOrRear(side) ? '0.8rem' : '0.5rem';
+  //     marker.style.borderRadius = '0.8rem';
+  //     marker.style.transform = `rotate(${orientationDent})`;
+  //   }
+  // }
+  // if (length === 'big') {
+  //   if (shape === 'nonagon') {
+  //     marker.style.width = isFrontOrRear(side) ? '2.6rem' : '1.6rem';
+  //     marker.style.height = isFrontOrRear(side) ? '2.6rem' : '1.6rem';
+  //   } else if (shape === 'line') {
+  //     marker.style.width = isFrontOrRear(side) ? '2.9rem' : '2.2rem';
+  //     marker.style.height = isFrontOrRear(side) ? '1.2rem' : '0.8rem';
+  //     marker.style.borderRadius = '0.8rem';
+  //     marker.style.transform = `rotate(${orientationDent})`;
+  //   }
+  // }
 
   image.appendChild(marker);
 };
