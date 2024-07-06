9bb37cbbe287dac0677064f2f0b69f6d1f6bfd0b
Jev Lap <jevenghi@gmail.com>
2024-07-06 14:34:35 +0200

    console error added for photo upload fails

--- public/js/index.js
+++ public/js/index.js
@@ -258,6 +258,7 @@ if (fileInput) {
     } catch (error) {
       spinner.style.display = 'none';
       fileInput.value = '';
+      console.error(error);
       return showAlert('error', 'Error uploading photo. Please try again.');
     }
     if (logoImage) {
