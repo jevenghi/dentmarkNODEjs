7db38499b9319efa481c2a2dec31be3218915e19
Jevgenij Lapsin <jevenghi@gmail.com>
2026-07-04 14:18:08 +0200

    language buttons labels changed. default nl

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -68,7 +68,7 @@ exports.getLanding = catchAsyncError(async (req, res, next) => {
   const detectedLanguage =
     requestedLanguage === 'nl' || requestedLanguage === 'en'
       ? requestedLanguage
-      : req.acceptsLanguages('nl', 'en') || 'en';
+      : 'nl';
   const language = detectedLanguage === 'nl' ? 'nl' : 'en';
 
   res.status(200).render('landing', {
