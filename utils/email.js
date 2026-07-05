8cdd3d5dbc1b336b6bf971e61aef586da18b5ea0
Jevgenij Lapsin <jevenghi@gmail.com>
2026-07-05 11:52:24 +0200

    nodemailer trtansport fix

--- utils/email.js
+++ utils/email.js
@@ -11,7 +11,8 @@ class Email {
   async send(subject, message, attachments = []) {
     const transport = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      port: Number(process.env.EMAIL_PORT || 587),
+      secure: false,
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,
