5ee1032743698a89c1bca8f2ee95a55837e5c38d
Jevgenij Lapsin <jevenghi@gmail.com>
2026-07-06 19:54:34 +0200

    from email changed

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,7 @@ const htmlToText = require('html-to-text');
 class Email {
   constructor(userEmail) {
     this.to = userEmail;
-    this.from = 'Dentmarker App <info@crateofrare.com>';
+    this.from = 'Dentmarker App <info@am-place.com>';
   }
 
   async send(subject, message, attachments = []) {
@@ -43,7 +43,7 @@ const sendMail = async (options) => {
   });
 
   const mailOptions = {
-    from: 'Dentmarker App <info@crateofrare.com>',
+    from: 'Dentmarker App <info@am-place.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
