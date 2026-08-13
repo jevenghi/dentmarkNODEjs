bd227901ec815a9887dad1bde56bf3ed95271720
Jevgenij Lapsin <jevenghi@gmail.com>
2026-08-13 09:40:19 +0200

    email address change to jev

--- utils/email.js
+++ utils/email.js
@@ -6,7 +6,7 @@ class Email {
   constructor(userEmail) {
     this.to = userEmail;
     // this.from = 'Dentmarker App <info@am-place.com>';
-    this.from = 'Dentmarker App <audiuka@gmail.com>';
+    this.from = 'Dentmarker App <jevenghi@gmail.com>';
 
   }
 
@@ -46,7 +46,7 @@ const sendMail = async (options) => {
 
   const mailOptions = {
     // from: 'Dentmarker App <info@am-place.com>',
-    from: 'Dentmarker App <audiuka@gmail.com>',
+    from: 'Dentmarker App <jevenghi@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message,
